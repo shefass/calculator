0ab365f34dbe5d87cafce65ee903e3d0105153d7
Shefass <shefass@gmail.com>
2019-02-28 22:54:49 +0200

    Updates

---
+++ precache-manifest.ca844b888e6cc941d231b7b20313de3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d721d06f78c96d55f57",
+    "url": "/calculator/static/js/runtime~main.0d721d06.js"
+  },
+  {
+    "revision": "b3a20647b6b3f34c219d",
+    "url": "/calculator/static/js/main.b3a20647.chunk.js"
+  },
+  {
+    "revision": "4551f0595bc8e9284ea0",
+    "url": "/calculator/static/js/2.4551f059.chunk.js"
+  },
+  {
+    "revision": "b3a20647b6b3f34c219d",
+    "url": "/calculator/static/css/main.1c3a2f79.chunk.css"
+  },
+  {
+    "revision": "e2a584c11702124476e57ecc9673f554",
+    "url": "/calculator/index.html"
+  }
+];
